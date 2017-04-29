bcdc6544a23d300ae5e4098ba0e0cf48021f0f7f
shilong.sang <i5ting@126.com>
2017-04-29 11:14:38 +0800

    lazycommmit, please ignore

--- chapter01/hello_node.js
+++ chapter01/hello_node.js
@@ -2,8 +2,11 @@ const http = require('http')
 const PORT = 3000
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
-}).listen(PORT);
+  // 设置Response状态码和Headers
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  
+  // 设置Response向浏览器写入内容
+  res.end('Hello Node.js\n')
+}).listen(PORT)
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+console.log(`Server running at http://127.0.0.1:${PORT}/`)
