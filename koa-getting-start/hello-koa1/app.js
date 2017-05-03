6b0269066f7b2ee356caa7c55f57748161be494b
shilong.sang <i5ting@126.com>
2017-05-03 14:24:24 +0800

    lazycommmit, please ignore

---
+++ koa-getting-start/hello-koa1/app.js
@@ -0,0 +1,23 @@
+const koa = require('koa')
+const app = koa()
+
+// logger
+app.use(function *loggermildleware(next){
+  let start = new Date
+
+  console.log('[logger middleware] before yield...')
+  yield next
+  console.log('[logger middleware] after yield...')
+  
+  let ms = new Date - start
+  console.log('%s %s - %s', this.method, this.url, ms)
+})
+
+// response
+app.use(function *responsemildleware(){
+  console.log('[response middleware] after yield...')
+
+  this.body = 'Hello World'
+})
+
+app.listen(3000)
