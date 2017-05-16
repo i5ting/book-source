732c8420c37940db4144a6efbc6c17233657ce7c
shilong.sang <i5ting@126.com>
2017-05-17 06:32:17 +0800

    lazycommmit, please ignore

--- koa-practice/request-demo/routes/upload.js
+++ koa-practice/request-demo/routes/upload.js
@@ -3,17 +3,17 @@ const multer = require('koa-multer');
 const upload = multer({ dest: 'uploads/' });
 
 router.prefix('/upload')
-router.post('/', upload.single('avatar'), (ctx) => {
+
+router.post('/', upload.any(), (ctx) => {
 	ctx.body = {
 		status: {
 			code: 0,
 			msg: 'upload sucess'
 		},
 		data: {
-			
+			body: ctx.req.body
 		}
 	}
 })
-
 
 module.exports = router
