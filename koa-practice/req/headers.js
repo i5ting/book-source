3c35b2ea3c13f0798999a16f82e5044d2482faaf
shilong.sang <i5ting@126.com>
2017-05-16 14:34:08 +0800

    lazycommmit, please ignore

---
+++ koa-practice/req/headers.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async ctx => {
+  return ctx.body = {
+    header: ctx.header,
+    get_cache_control  : ctx.get('Cache-Control')
+  }
+})
+
+app.listen(3000)
